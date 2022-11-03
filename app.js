55ee6ca45d9b9e75f13d87566beabaccfd5d5ea7
Kai <yingkai.ma.c@gmail.com>
2022-11-02 22:18:29 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//require modules
+const express = require('express');
+const morgan = require('morgan');
+const indexRoutes = require('./routes/indexRoutes');
+const stateRoutes = require('./routes/stateRoutes');
+const parkRoutes = require('./routes/parkRoutes');
+const methodOverride = require('method-override');
+//create  app
+const app = express();
+
+
+
+//configure app
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+//moutn middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+//set up routes
+app.use('/', indexRoutes);
+app.use('/states', stateRoutes);
+app.use('/parks', parkRoutes);
+
+//start the server
+app.listen(port, host, () => {
+    console.log('The server is runing no', port);
+});
